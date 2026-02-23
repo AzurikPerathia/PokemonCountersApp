3198dd19156bad132b6220f69c4e63a793f160db
AzurikRiseOfPerathia <azurikriseofperathia@gmail.com>
2026-02-23 01:06:11 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 950,
+    resizable: true
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
